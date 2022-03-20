e817210a97c9fac12053df2bbc32f5e35ebcf77f
Avik Kundu <1828008@kiit.ac.in>
2022-03-20 21:56:06 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "Hello, World v1!")
 	})
 	if os.Getenv("PORT") != "" {
 		port := os.Getenv("PORT")
